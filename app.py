fb44c46e236c5dc2dacd056dc630c526ca83323b
Jose Ulisses <joseulisses03@gmail.com>
2025-06-16 22:40:29 -0300

    dict pra guardar as urls

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 # Por enquanto vamos guardar as URLs em memória em um JSON
+url_db = {
+    "aBc1": "https://www.google.com/",
+    "xYz9": "https://docs.python.org/3/"
+}
 
 @app.route("/")
 def index():
@@ -12,4 +16,12 @@ def index():
 def add_url():
     data = request.get_json()
     url = data['url']
-    return jsonify(url)
+    if not url:
+        return jsonify({"erro": "URL não fornecida"}), 400
+    codigo_curto = "testeExemplo"
+    url_db[codigo_curto] = url
+    url_curta = request.host_url + codigo_curto
+    return jsonify({
+        "url_original": url,
+        "url_curta": url_curta
+    })
