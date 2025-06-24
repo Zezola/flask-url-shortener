487235b0ec81bae11cd8cdac225f06c0597e27c2
Jose Ulisses <joseulisses03@gmail.com>
2025-06-24 08:41:54 -0300

    Adicionando uma URL no bd quando damos submit

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask, request, jsonify, render_template
-import uuid
+from flask import Flask, request, jsonify, render_template, url_for,flash, redirect
+from hashids import Hashids
+import sqlite3
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'this should be a secret random string'
 
+hashids = Hashids(min_length=4, salt=app.config['SECRET_KEY'])
 
-@app.route("/")
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return jsonify({'name': 'Pedro'})
+    conn = get_db_connection()
 
-@app.route("/url", methods=['GET','POST'])
-def add_url():
+    if request.method == 'POST':
+        url = request.form['url']
+
+        if not url:
+            flash('URL is required')
+            return redirect(url_for('index'))
+        
+        url_data = conn.execute('INSERT INTO urls (original_url) VALUES (?)', (url,))
+        conn.commit()
+        conn.close()
+
+        url_id = url_data.lastrowid
+        hashid = hashids.encode(url_id)
+        short_url = request.host_url + hashid
+        
+        return render_template('index.html', short_url = short_url)
     return render_template('index.html')
