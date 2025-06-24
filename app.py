192e0df4c4448d1101aa21d948b706c2964d1ab1
Jose Ulisses <joseulisses03@gmail.com>
2025-06-24 08:01:13 -0300

    Criando template base e index

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import uuid
 
 app = Flask(__name__)
 
-# Por enquanto vamos guardar as URLs em memória em um JSON
-url_db = {
-    "aBc1": "https://www.google.com/",
-    "xYz9": "https://docs.python.org/3/"
-}
 
 @app.route("/")
 def index():
     return jsonify({'name': 'Pedro'})
 
-@app.route("/url", methods=['POST'])
+@app.route("/url", methods=['GET','POST'])
 def add_url():
-    data = request.get_json()
-    url = data['url']
-    if not url:
-        return jsonify({"erro": "URL não fornecida"}), 400
-    codigo_curto = "testeExemplo"
-    url_db[codigo_curto] = url
-    url_curta = request.host_url + codigo_curto
-    return jsonify({
-        "url_original": url,
-        "url_curta": url_curta
-    })
+    return render_template('index.html')
